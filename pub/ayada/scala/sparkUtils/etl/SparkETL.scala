4891ff77eea4d7a80c5c3e2579486184bd5cb821
k-ayada <kiran14n@gmail.com>
2017-08-30 15:50:07 +0530

    Update SparkETL.scala

--- pub/ayada/scala/sparkUtils/etl/SparkETL.scala
+++ pub/ayada/scala/sparkUtils/etl/SparkETL.scala
@@ -1,141 +1,176 @@
 package pub.ayada.scala.sparkUtils.etl
 import java.time.Duration
-import java.util.concurrent.CountDownLatch
-import java.util.concurrent.Semaphore
-import java.util.concurrent.Executors
 import scala.xml.Elem
-import scala.concurrent._
+
+import org.apache.spark.SparkConf
+import org.apache.spark.SparkContext
+
+import pub.ayada.scala.utils.hdfs.HdfsFileIO
+import pub.ayada.scala.utils.DateTimeUtils
+import pub.ayada.scala.utils.ObjectUtils
+
 import pub.ayada.scala.sparkUtils.etl.SparkETL._
 import pub.ayada.scala.utils.DateTimeUtils._
 
-class SparkETL(args: Array[String]) {
-    /*
-     * xml DOM object of the input xml file. Used for running the xpath queries
-     */
-    @throws(classOf[Exception])
-    private lazy val jobConfXML: scala.xml.Elem = {
-        val arg = args.sliding(2, 1).toList
-        var xml: scala.xml.Elem = null
-        args.sliding(2, 1).toList.collect {
-            case Array("--JobConf", jobConf: String) =>
-                println(pub.ayada.scala.utils.DateTimeUtils.getFmtDtm("Driver-Main") + "--JobConf: " + jobConf)
-                val reader = new java.io.InputStreamReader(pub.ayada.scala.utils.hdfs.HdfsFileIO.getHdfsFlInputStream(jobConf), "UTF-8")
-                xml = scala.xml.XML.load(reader)
+class SparkETL(args : Array[String]) {
+
+    private lazy val argsMap : scala.collection.mutable.Map[String, String] = {
+        val map : scala.collection.mutable.Map[String, String] = scala.collection.mutable.Map[String, String]()
+        args.sliding(2, 1).toList.collect { case Array(key : String, value : String) => map.put(key, value) }
+        map
+    }
+
+    // xml DOM object of the input xml file. Used for running the xpath queries
+    private lazy val jobConfXML : scala.xml.Elem = {
+        val xmlPath = argsMap.getOrElse("--JobConf", null)
+        if ("" == xmlPath) {
+            throw new java.lang.Exception(DateTimeUtils.getFmtDtm("Driver-Main") + "Failed to parse the commanline. Need the parameter --JobConf \"<path to xml file>\" ")
         }
+        println(DateTimeUtils.getFmtDtm("Driver-Main") + "--JobConf: " + xmlPath)
+        val xml : scala.xml.Elem = scala.xml.XML.load(new java.io.InputStreamReader(HdfsFileIO.getHdfsFlInputStream(xmlPath), "UTF-8"))
         if (xml == null) {
-            throw new java.lang.Exception(pub.ayada.scala.utils.DateTimeUtils.getFmtDtm("Driver-Main") + "Failed to parse the commanline. Need the parameter --JobConf \"<path to xml file>\" ")
+            throw new java.lang.Exception(DateTimeUtils.getFmtDtm("Driver-Main") + "Failed to parse the commanline. Need the parameter --JobConf \"<path to xml file>\" ")
         }
         xml
     }
+
     private lazy val AppName = (jobConfXML \ "@appName").text
-    private lazy val maxThread = (jobConfXML \ "@maxNumberOfThreads").text.toInt
+    private lazy val defaults : scala.collection.mutable.Map[String, String] = scala.collection.mutable.Map[String, String]()
+
     /*
      * Spark Context
      */
-    private lazy val sparkContext: org.apache.spark.SparkContext = {
-        val cnf = new org.apache.spark.SparkConf()
+    private lazy val sparkContext : SparkContext = {
+        val cnf = new SparkConf()
         cnf.setAppName(AppName)
             .set("spark.scheduler.mode", "FAIR")
-            .setIfMissing("hive.execution.engine", "spark")
-        //.set("spark.driver.allowMultipleContexts", "true")
-
+            .set("spark.sql.hive.convertMetastoreParquet", "false")
+            .set("spark.sql.parquet.mergeSchema", "false")
+            .set("spark.sql.parquet.int96AsTimestamp", "true")
+            .set("spark.shuffle.service.enabled", "true")
+            .set("hive.execution.engine", "spark")
+            .set("spark.driver.allowMultipleContexts", "false")
+            .set("spark.rpc.io.serverThreads", "32")
+            .set("spark.serializer", "org.apache.spark.serializer.KryoSerializer")
+            .set("yarn.nodemanager.vmem-check-enabled", "false")
+        if (argsMap.contains("fs.s3a.access.key") && argsMap.contains("fs.s3a.secret.key")) {
+            cnf.set("fs.s3a.access.key", argsMap.getOrElse("fs.s3a.access.key", null))
+                .set("fs.s3a.secret.key", argsMap.getOrElse("fs.s3a.secret.key", null))
+        }
         val sc = new org.apache.spark.SparkContext(cnf)
         sc.setLogLevel("ERROR")
         sc
     }
-    /*
-     * List of properties passed in the input properties file provided in the
-     * config file
-     */
-    private lazy val jobProps: List[pub.ayada.scala.sparkUtils.etl.JobProps] = {
+    defaults.put("defaultNoOfDFPartitions", {
+        if ("" != (jobConfXML \ "@defaultNoOfPartitions").text.trim)
+            (jobConfXML \ "@defaultNoOfPartitions").text.trim
+        else {
+            {
+                sparkContext.getConf.getInt("spark.executor.instances", 5) *
+                    sparkContext.getConf.getInt("spark.executor.cores", 1) * 2
+            }.toString
+        }
+    })
 
-        var jobProps: List[pub.ayada.scala.sparkUtils.etl.JobProps] = List()
+    defaults.put("defaultStorageLevel", {
+        if ("" != (jobConfXML \ "@defaultStorageLevel").text.trim)
+            (jobConfXML \ "@defaultStorageLevel").text.trim
+        else
+            "MEMORY_AND_DISK"
+    })
+
+    //List of properties passed in the input properties file provided in the config file
+    private lazy val jobProps : List[JobProps] = {
+
+        var jobProps : scala.collection.mutable.ListBuffer[JobProps] = scala.collection.mutable.ListBuffer()
         val printer = new scala.xml.PrettyPrinter(250, 4)
 
-        (jobConfXML \ "_").foreach(job => {
-            val sb = new StringBuilder()
-            sb.append(pub.ayada.scala.utils.DateTimeUtils.getFmtDtm("Driver-Main") + " Job: " + (job \ "@id").text.trim + " LoadType: " + job.label).append("\n")
-            sb.append(pub.ayada.scala.utils.DateTimeUtils.getFmtDtm("Driver-Main") + " Input Config:\n" + printer.format(job))
+        (jobConfXML \ "job").foreach(job => {
 
             val jobId = (job \ "@id").text.trim
-            job.label match {
-                case "jdbc2hive" => {
-                    val h = pub.ayada.scala.sparkUtils.etl.DF2Hive.getDF2HiveProps(job)
-                    if (h.size == 0) {
-                        println(pub.ayada.scala.utils.DateTimeUtils.getFmtDtm("Driver-Main") + "Err: Failed to retrive properties to persit final data in Hive. Skippig the ETL. JobID:" + jobId)
-                    } else {
-                        val p = pub.ayada.scala.sparkUtils.etl.Jdbc2DF.getJdbc2DFProps(job, sparkContext)
-                        val t = getTransformers((job \ "transformers"))
-                        jobProps = jobProps :+ pub.ayada.scala.sparkUtils.etl.JdbcJobProps(id = jobId, readProps = p, dF2HiveProps = h, transformProps = t)
-                        sb.append("\n").append(pub.ayada.scala.utils.DateTimeUtils.getFmtDtm("Driver-Main") + "JDBC Properties for job: " + jobId + "\n" + pub.ayada.scala.utils.ObjectUtils.prettyPrint(p))
-                        sb.append("\n").append(pub.ayada.scala.utils.DateTimeUtils.getFmtDtm("Driver-Main") + "Transformation Properties for job: " + jobId + "\n" + pub.ayada.scala.utils.ObjectUtils.prettyPrint(for (x <- t) yield { x }))
-                        sb.append("\n").append(pub.ayada.scala.utils.DateTimeUtils.getFmtDtm("Driver-Main") + "Hive Properties for job: " + jobId + "\n" + pub.ayada.scala.utils.ObjectUtils.prettyPrint(h))
-                    }
+
+            val sb = new StringBuilder()
+            sb.append(DateTimeUtils.getFmtDtm("Driver-Main")).append(" Job: ").append(jobId).append("\n")
+
+            sb.append(DateTimeUtils.getFmtDtm("Driver-Main")).append(" Input Config:\n").append(printer.format(job))
+
+            val WriteProps = JobProps.getDF2TargetProps((job \ "target"), sparkContext, defaults, sb)
+
+            // If we failed to determine at least one Target, no point in proceeding with the further processing.
+            if (WriteProps.size == 0) {
+                println(DateTimeUtils.getFmtDtm("Driver-Main") + "Err: Failed to retrive properties to persit final data in Hive. Skippig the ETL. JobID:" + jobId)
+            } else {
+                //retrieve and report Source properties
+                val src2DFProps = JobProps.getSrc2DFProps((job \ "src"), sparkContext, defaults, sb)
+                sb.append("\n\n").append(DateTimeUtils.getFmtDtm("Driver-Main") + "Read Tasks of job: " + jobId)
+                for (pp <- src2DFProps) {
+                    sb.append("\n\t\t")
+                        .append("Source Type: ")
+                        .append(pp._1)
+                        .append(" -> ")
+                        .append("Property: ")
+                        .append(pp._2.toString)
                 }
-                case "csv2hive" => {
-                    val h = pub.ayada.scala.sparkUtils.etl.DF2Hive.getDF2HiveProps(job)
-                    if (h.size == 0) {
-                        println(pub.ayada.scala.utils.DateTimeUtils.getFmtDtm("Driver-Main") + "Err: Failed to retrive properties to persit final data in Hive. Skippig the ETL. JobID:" + jobId)
-                    } else {
-                        val p = pub.ayada.scala.sparkUtils.etl.Csv2DF.getCsv2DFProps(job)
-                        val t = getTransformers((job \ "transformers"))
-                        jobProps = jobProps :+ pub.ayada.scala.sparkUtils.etl.CsvJobProps(id = jobId, readProps = p, dF2HiveProps = h, transformProps = t)
-                        sb.append("\n").append(pub.ayada.scala.utils.DateTimeUtils.getFmtDtm("Driver-Main") + "CSV Properties for job: " + jobId + ": " + pub.ayada.scala.utils.ObjectUtils.prettyPrint(p))
-                        sb.append("\n").append(pub.ayada.scala.utils.DateTimeUtils.getFmtDtm("Driver-Main") + "Transformation Properties for job: " + jobId + ": " + pub.ayada.scala.utils.ObjectUtils.prettyPrint(t))
-                        sb.append("\n").append(pub.ayada.scala.utils.DateTimeUtils.getFmtDtm("Driver-Main") + "Hive Properties for job: " + jobId + ": " + pub.ayada.scala.utils.ObjectUtils.prettyPrint(h))
-                    }
+
+                //retrieve and report  Transformer properties
+                val transformerProps = JobProps.getTransformers((job \ "transformers"), defaults)
+                sb.append("\n")
+                    .append(DateTimeUtils.getFmtDtm("Driver-Main"))
+                    .append("Transformation Properties for job: " + jobId)
+                for (tt <- transformerProps) {
+                    sb.append("\n\t\t")
+                        .append(tt.toString)
                 }
-                case _ => {
-                    sb.append("\n").append("Err:Skipping the job:").append(jobId).append(". Reason: Unknown Job type").append(job.label).append("received. Valid values : jdbc2hive | csv2hive")
+
+                //report Target Properties
+                sb.append("\n").append(DateTimeUtils.getFmtDtm("Driver-Main"))
+                    .append("Target Properties for job: " + jobId)
+                for (hh <- WriteProps) {
+                    sb.append("\n\t\t")
+                        .append(ObjectUtils.prettyPrint(hh))
                 }
+
+                jobProps += JobProps(id = jobId, readProps = src2DFProps, writeProps = WriteProps, transformProps = transformerProps)
             }
+            sb.append("\n")
             println(sb.toString)
         })
 
-        println(pub.ayada.scala.utils.DateTimeUtils.getFmtDtm("Driver-Main") + "# jobs to process: " + jobProps.size)
-        jobProps
-    }
-
-    def getTransformers(xferNode: scala.xml.NodeSeq): List[pub.ayada.scala.sparkUtils.etl.TransformerProps] = {
-        var res: List[pub.ayada.scala.sparkUtils.etl.TransformerProps] = List()
-        //println(pub.ayada.scala.utils.DateTimeUtils.getFmtDtm("Driver-Main") + "Transformers:" + new scala.xml.PrettyPrinter(250, 4).formatNodes(xferNode))
-        (xferNode \ "transformer").foreach(x => {
-            res = res :+ pub.ayada.scala.sparkUtils.etl.Transformer.getTransformerProps(x)
-        })
-        res
+        println(DateTimeUtils.getFmtDtm("Driver-Main") + "# jobs to process: " + jobProps.size)
+        jobProps.toList
     }
 
     /**
-     * Starts off the load process.
-     */
-    def kickoffLoad(): Int = {
+      * Starts off the load process.
+      */
+    def kickoffLoad() : Int = {
         if (sparkContext == null) {
             throw new Exception("Spark Context is not yet initialized... :(")
         }
 
         val status = new scala.collection.mutable.HashMap[String, Int]()
         jobProps.foreach { p =>
-            println(pub.ayada.scala.utils.DateTimeUtils.getFmtDtm("Driver-Main") + "Processing JobID : " + p.id)
+            println(DateTimeUtils.getFmtDtm("Driver-Main") + "Processing JobID : " + p.id)
             status.put(p.id, 0)
-            val j = new pub.ayada.scala.sparkUtils.etl.Executor(sparkContext, p, status, maxThread)
+            val j = new Executor(sparkContext, p, defaults, status)
             j.process()
         }
 
         var rc = 0
         for ((k, v) <- status) {
-            println(pub.ayada.scala.utils.DateTimeUtils.getFmtDtm("Driver-Main") + "RC for job : " + k + " is : " + v)
+            println(DateTimeUtils.getFmtDtm("Driver-Main") + "RC for job : " + k + " is : " + v)
             if (0 != v) rc += v
         }
-        println(pub.ayada.scala.utils.DateTimeUtils.getFmtDtm("Driver-Main") + "Final RC is : " + rc)
+        println(DateTimeUtils.getFmtDtm("Driver-Main") + "Final RC is : " + rc)
         rc
     }
 }
 
 object SparkETL {
-    def main(args: Array[String]): Unit = {
+    def main(args : Array[String]) : Unit = {
         val run = args(0)
         var rc = 0
-        val sparkETL: SparkETL = new SparkETL(args)
+        val sparkETL : SparkETL = new SparkETL(args)
         rc = sparkETL.kickoffLoad
 
         if (rc != 0)
